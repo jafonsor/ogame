f1d4f598743596637e8f328b853713be0ec56bbf
João Rodrigues <jrodriguesnum1@hotmail.com>
2014-01-26 16:47:52 +0000

    Now it is possible to chose the licitation material.

--- leilao.js
+++ leilao.js
@@ -1,6 +1,10 @@
 // constants
 var LICIT_DELAY = 500;  // milliseconds
 var LICITATION_LIMIT = 20000; //max bet
+var MATERIAL_CLASSES = [ 'js_sliderMetalMax', 'js_sliderCrystalMax', 'js_sliderDeuteriumMax' ];
+var METAL     = 0;
+var CRISTAL   = 1;
+var DEUTIRIUM = 2;
 
 //auxiliar functions
 function playerName() {
@@ -15,8 +19,10 @@ function licitantName() {
   return $("a[class='detail_value odd currentPlayer']").html().trim();
 }
 
+var licitation_material = METAL;
 function licita() {
-  $("[class='value-control max js_sliderMetalMax js_valButton tooltipRight js_hideTipOnMobile']").click();
+  console.log("[class='value-control max " + MATERIAL_CLASSES[licitation_material] + " js_valButton tooltipRight js_hideTipOnMobile']");
+  $("[class='value-control max " + MATERIAL_CLASSES[licitation_material] + " js_valButton tooltipRight js_hideTipOnMobile']").click();
   $(".pay").click();
 }
 
@@ -24,11 +30,15 @@ function licita() {
 var keepLicitar = false;
 setInterval(
  function() {
-
+   
    if(keepLicitar) {
+     console.log("licitando: " + licitantName() + ", " + playerName());
      if(licitantName() != playerName()) {
-       if(actualLicitation() < LICITATION_LIMIT)
+       console.log("outro licitou mais alto");
+       if(actualLicitation() < LICITATION_LIMIT) {
+         console.log(actualLicitation() + ', ' + LICITATION_LIMIT);
          licita();
+       }
      }
    }
    
